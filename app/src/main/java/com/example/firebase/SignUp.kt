e5ac0c936e5d5ceb218b180ccd620b678dc3930f
MohamedMohamed <m01126165016@icloud.com>
2024-06-23 10:20:57 +0300

    Firebase Authentication With Email

--- app/src/main/java/com/example/firebase/SignUp.kt
+++ app/src/main/java/com/example/firebase/SignUp.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.firebase.databinding.ActivityMainBinding
 import com.example.firebase.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
